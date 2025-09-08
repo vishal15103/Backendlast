01a459d0ad572b146c1a8dbcc73aff250ee7d8ab
vishal15103 <121659782+vishal15103@users.noreply.github.com>
2025-09-08 23:46:54 +0530

    Create Backendapplication.java

---
+++ Backendapplication.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+
+public class BackendlastApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BackendlastApplication.class, args);
+
+    }
+
+}
